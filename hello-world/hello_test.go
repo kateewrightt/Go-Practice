d9bdeaf2602d2aa0e6300e364470cd2136f4d64c
kateewrightt <wrighk@outlook.com>
2024-07-23 12:01:01 +1000

    Hello World with tests

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to myself", func(t *testing.T) {
-		got := Hello("Kate")
+		got := Hello("Kate", "English")
 		want := "Hello, Kate"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Kate", "Spanish")
+		want := "Hola, Kate"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Kate", "French")
+		want := "Bonjour, Kate"
 		assertCorrectMessage(t, got, want)
 	})
 
